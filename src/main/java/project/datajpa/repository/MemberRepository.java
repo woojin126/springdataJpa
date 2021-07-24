351cbbd0a4e4eb1db556d286d8da4d7eacbfea53
woojin126 <woojin126@naver.com>
2021-07-24 23:49:51 +0900

    맴버 repository JpaRepository 상속

--- src/main/java/project/datajpa/repository/MemberRepository.java
+++ src/main/java/project/datajpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package project.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.datajpa.entity.Member;
 
+/**
+ * 인터페이스끼리 상속했는데 어떻게 기능이 동작을할까? 테스트코드에서 찍어보자
+  , 테스트 코드에가보면 나는 @Autowired로 주입을 한것의 정체는? 프록시 객체.
+ */
 public interface MemberRepository extends JpaRepository<Member,Long>
 {
 
