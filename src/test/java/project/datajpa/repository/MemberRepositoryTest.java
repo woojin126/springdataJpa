4548ea9734f17256622a0244afdd304ab49e2189
woojin126 <woojin126@naver.com>
2021-07-24 16:57:06 +0900

    MemberRepository Test

---
+++ src/test/java/project/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package project.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.stubbing.answers.ThrowsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //== 비교
+    }
+
+}
