4165183c705e299d2c9da3c3b4fca251e10d6853
woojin126 <woojin126@naver.com>
2021-07-24 18:44:43 +0900

    Member entity  연관관계 추가, 양방향 편리메서드 추가

--- src/main/java/project/datajpa/entity/Member.java
+++ src/main/java/project/datajpa/entity/Member.java
@@ -1,34 +1,50 @@
 package project.datajpa.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})//팀을(연관관계필드) 출력하면 큰일남 무한루프일어남
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member(){//아무대서나 호출되지않도록, 엔티티는 기본적으로 기본생성자가 필요
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+   /* protected Member(){//아무대서나 호출되지않도록, 엔티티는 기본적으로 기본생성자가 필요
         
-    }
+    }*/
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+    }
+
     //세터대신 그냥
-    public void changeUsername(String username){
-        this.username=username;
+    public void changeUsername(String username) {
+        this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/main/java/project/datajpa/entity/Team.java
@@ -0,0 +1,2 @@
+package project.datajpa.entity;public class Team {
+}
