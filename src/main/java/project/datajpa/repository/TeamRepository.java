04340a5e69bcb68cb2ef331d80806bb05392da5e
woojin126 <woojin126@naver.com>
2021-07-24 23:50:29 +0900

    TeamRepository  JpaRepository 상속

--- src/main/java/project/datajpa/repository/TeamRepository.java
+++ src/main/java/project/datajpa/repository/TeamRepository.java
@@ -1,2 +1,12 @@
-package project.datajpa.repository;public interface TeamRepository {
+package project.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.datajpa.entity.Team;
+
+/**
+ * JpaRepositort 가보이면 알아서 구현체를 만들어서 프록시로 꽂아버림
+ */
+public interface TeamRepository extends JpaRepository<Team,Long> {
+
+    
 }
