d9c9c73606400392c5a1917c30bea4c091e7f245
woojin126 <woojin126@naver.com>
2021-07-25 16:48:38 +0900

    순수 JPA 페이징 추가

--- src/main/java/project/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/project/datajpa/repository/MemberJpaRepository.java
@@ -42,5 +42,36 @@ public class MemberJpaRepository {
         return em.find(Member.class, id);
     }
 
+    public List<Member> findByUsernameAndAgeGreaterThen(String username, int age){
 
+        return em.createQuery("select m from Member m where m.username =:username and m.age > :age", Member.class)
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+    }
+
+    //네임드쿼리 호출 실무에서 사용 x
+    public List<Member> findByUsername(String username){
+
+       return em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    //순수 JPA 페이징
+    //방언이라는게 존재하기때문에 db가 바뀌어도 쿼리가 알아서 바뀌어서 나감
+    public List<Member> findByPage(int age, int offset, int limit){
+       return  em.createQuery("select m from Member m where m.age = :age order by m.username desc ",Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age){
+        return em.createQuery("select count(m) from Member m where m.age = :age ", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+
+    }
 }
