dabe80387e3928194baa9901b57881fc7322706b
woojin126 <woojin126@naver.com>
2021-07-25 16:48:22 +0900

    @NamedQuery 추가

--- src/main/java/project/datajpa/entity/Member.java
+++ src/main/java/project/datajpa/entity/Member.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id","username","age"})//팀을(연관관계필드) 출력하면 큰일남 무한루프일어남
+@NamedQuery( //잘안씀 ,진짜 좋은장점하나 애플리케이션 로딩시점에 아래 쿼리를 파싱해서 오류가있으면 알려줌
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
@@ -36,6 +40,11 @@ public class Member {
         if (team != null){
             changeTeam(team);
         }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     //세터대신 그냥
