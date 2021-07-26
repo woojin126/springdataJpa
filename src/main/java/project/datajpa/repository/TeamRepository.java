6e8787bbd38fac1e0cdd0295ca6d58a754c28b6b
woojin126 <woojin126@naver.com>
2021-07-27 03:19:44 +0900

    spirng Data JPA repository @Querty @EntityGraph 동시사용

--- src/main/java/project/datajpa/repository/TeamRepository.java
+++ src/main/java/project/datajpa/repository/TeamRepository.java
@@ -1,12 +1,21 @@
 package project.datajpa.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import project.datajpa.entity.Team;
+
+import java.util.List;
 
 /**
  * JpaRepositort 가보이면 알아서 구현체를 만들어서 프록시로 꽂아버림
  */
 public interface TeamRepository extends JpaRepository<Team,Long> {
 
+
+/*    @Query("select t from Team t " +
+            "join fetch t.members")*/
+@EntityGraph(attributePaths = {"members"})
+    List<Team> findEntityGraphByName(String name);
     
 }
