05ba9e14807234aaf66136a9b2d1c673a49da302
woojin126 <woojin126@naver.com>
2021-07-27 03:20:03 +0900

    Spring Data Jpa TestCode

--- src/test/java/project/datajpa/repository/TeamRepositoryTest.java
+++ src/test/java/project/datajpa/repository/TeamRepositoryTest.java
@@ -1,4 +1,55 @@
+package project.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.datajpa.entity.Member;
+import project.datajpa.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class TeamRepositoryTest {
-  
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+
+    @Test
+    void teams(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        List<Team> teamA1 = teamRepository.findEntityGraphByName("teamA");
+        for (Team team : teamA1) {
+            System.out.println("team = " + team);
+            for(Member member : team.getMembers()){
+                System.out.println("member = " + member.getUsername());
+            }
+        }
+    }
+
 }
