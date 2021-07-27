0fca0f5d65c7e2ae56d0aed1bbb677708ffd8d30
woojin126 <woojin126@naver.com>
2021-07-27 19:19:05 +0900

    spring Data JPA CustomRepositoryImpl

--- src/main/java/project/datajpa/repository/MemberRepositoryImpl.java
+++ src/main/java/project/datajpa/repository/MemberRepositoryImpl.java
@@ -1,4 +1,26 @@
 package project.datajpa.repository;
 
-public class MemberRepositoryImpl {
+import lombok.RequiredArgsConstructor;
+import project.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * 규칙이있다 
+ * MemberRepository + Impl 규칙을 맞춰줘야 한다. 그래야 DATA JPA 가 알아서 조립을해서 CALL했을때 구현체를 찾아서 호출해준다
+ *---> 그런데 이렇게해도 어차피  MemberRepository에 다몰아 넣는것이라
+ * 핵심 비지니스로직과, 단순 화면 에 맞춘(dto를이용해 필요한 정보만 뽑는) 쿼리들을 분리하는게 낫다 (따로 클래스로 repository륾 만드는게 낫다 */
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Override
+    public List<Member> findMemberCustom() {
+        return em.createQuery("select m from Member m",Member.class )
+               .getResultList();
+    }
 }
