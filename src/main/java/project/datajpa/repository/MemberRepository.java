a7d3639329ab5ee06154f88c427160f941358ded
woojin126 <woojin126@naver.com>
2021-07-27 19:20:04 +0900

    extends spring Data JPA CustomRepository

--- src/main/java/project/datajpa/repository/MemberRepository.java
+++ src/main/java/project/datajpa/repository/MemberRepository.java
@@ -19,7 +19,7 @@ import java.util.Optional;
  * 인터페이스끼리 상속했는데 어떻게 기능이 동작을할까? 테스트코드에서 찍어보자
   , 테스트 코드에가보면 나는 @Autowired로 주입을 한것의 정체는? 프록시 객체.
  */
-public interface MemberRepository extends JpaRepository<Member,Long>
+public interface MemberRepository extends JpaRepository<Member,Long>, MemberRepositoryCustom
 {
     //만약 인터페이스에 정의되있는 기능말고 내가 만든 기능을 사용하고싶다
     //이렇게만 정의해놨는데 쿼리가 나감;; 이유는?
