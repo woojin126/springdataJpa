53a88e25b3810c9ae3d1277981889ace77ad5817
woojin126 <woojin126@naver.com>
2021-07-27 19:23:10 +0900

    MemberQeuryRepository  스프링데이터 리포지토리를 사용하지않고 순수 JPA로사용(커스텀방식은 공통으로사용하고 싶을때, 순수JPA 리포지토리 방식은 직접길고 어려운 쿼리를짜거나,단독적으로 사용되어야할때

--- src/main/java/project/datajpa/repository/MemberQueryRepository.java
+++ src/main/java/project/datajpa/repository/MemberQueryRepository.java
@@ -1,2 +1,23 @@
-package project.datajpa.repository;public class MemberQueryRepository {
+package project.datajpa.repository;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import project.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberQueryRepository {
+
+
+    @PersistenceContext
+    EntityManager em;
+    List<Member> findAllMembers(){
+       return  em.createQuery("select m from Member m ",Member.class)
+                .getResultList();
+    }
 }
