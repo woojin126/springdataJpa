73bce2ba5e0b62a770645d014b474449a68b4cdf
woojin126 <woojin126@naver.com>
2021-07-28 10:07:18 +0900

    수정,등록 시간 수정,등록자 TestCode

--- src/test/java/project/datajpa/entity/MemberTest.java
+++ src/test/java/project/datajpa/entity/MemberTest.java
@@ -1,13 +1,16 @@
 package project.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import project.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +22,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity(){
@@ -48,4 +54,54 @@ class MemberTest {
             System.out.println("=====>member = " + member.getTeam());
         }
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws InterruptedException {
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist 발생
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        System.out.println("findMember.getCreateDate() = " + findMember.getCreateDate());
+        System.out.println("findMember.getUpdateDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreateBy() = " + findMember.getCreateBy());
+        System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
+    }
+
+    @Test
+    public void 여러사용자생성마다시간생성() throws InterruptedException {
+        List<Member> members = makeList();
+
+        for (Member member : members) {
+            test1(member.getId());
+            Thread.sleep(1000);
+        }
+    }
+    public List<Member> makeList() {
+        List<Member> member = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Member memberd = new Member("woojin" + i);
+            member.add(memberd);
+        }
+        memberRepository.saveAll(member);
+        return member;
+    }
+
+    protected void test1(Long id){
+        Member member = memberRepository.findById(id).get();
+        member.setUsername("김우진");
+        em.flush();
+        //memberRepository.save(member);
+    }
+
+
+
+
 }
