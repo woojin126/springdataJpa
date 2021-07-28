e46494b773e214a92151d9ff2ba87134eea2c6da
woojin126 <woojin126@naver.com>
2021-07-28 10:07:59 +0900

    등록자, 수정자 를 알아내기위한 AuditorAware Bean 등록

--- src/main/java/project/datajpa/DataJpaApplication.java
+++ src/main/java/project/datajpa/DataJpaApplication.java
@@ -2,12 +2,36 @@ package project.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	/**
+	 * 세션이나, 시큐리티 에서 해당 사용자 아이디를 꺼내서 넣어준다
+	 * 그러면 해당아이디가 변경되거나, 생성될때마다 @CreateBy, @LastModifiedBy가 AuditorAware
+	 * 를 호출해서 결과물을 꺼내가서 사용한다.
+	 * */
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};
 	}
 
 }
