862f1c6fcc95c992aa1453fcc04c13aa009c0094
woojin126 <woojin126@naver.com>
2021-07-30 16:03:55 +0900

    Data  JPA  Member Specification class add

--- src/main/java/project/datajpa/repository/MemberSpec.java
+++ src/main/java/project/datajpa/repository/MemberSpec.java
@@ -1,2 +1,27 @@
-package project.datajpa.repository;public class MemberSpec {
+package project.datajpa.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.thymeleaf.util.StringUtils;
+import project.datajpa.entity.Member;
+
+import javax.persistence.criteria.*;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName){
+       return (root, query, criteriaBuilder) -> {
+
+           if(StringUtils.isEmpty(teamName)){
+               return null;
+           }
+
+           Join<Object, Object> t = root.join("team", JoinType.INNER);//회원과조인
+           return criteriaBuilder.equal(t.get("name"), teamName);
+       };
+    }
+
+    public static Specification<Member> username(final String username){
+        return (root, query, criteriaBuilder) ->
+            criteriaBuilder.equal(root.get("username"), username);
+    }
 }
