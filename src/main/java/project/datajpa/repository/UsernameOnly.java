ada73214f657a04805bb51abbaf925a6987411e3
woojin126 <woojin126@naver.com>
2021-07-30 16:04:40 +0900

    Projections interface add  (search Username)

--- src/main/java/project/datajpa/repository/UsernameOnly.java
+++ src/main/java/project/datajpa/repository/UsernameOnly.java
@@ -1,2 +1,6 @@
-package project.datajpa.repository;public interface UsernameOnly {
+package project.datajpa.repository;
+
+//Projections : 내가필요한 필드만 DTO로 뽑고싶을떄 사용
+public interface UsernameOnly {
+    String getUsername();
 }
